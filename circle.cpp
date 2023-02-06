ad4de8ef8290912e0d955bc71bbf1cc1edf6d095
Abrar Rahman <121895217+iamabrar14@users.noreply.github.com>
2023-02-07 01:04:34 +0600

    Create circle.cpp

---
+++ circle.cpp
@@ -0,0 +1,22 @@
+
+#include <graphics.h>
+
+//driver code
+int main()
+{
+
+	int gd = DETECT, gm;
+
+
+	initgraph(&gd, &gm, "");
+
+	// circle function
+	circle(250, 200, 50);
+
+	getch();
+
+
+	closegraph();
+
+	return 0;
+}
